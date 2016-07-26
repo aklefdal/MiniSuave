9a5df4424a914a4f957abaf3119ec1ad3b98e9d5
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2016-07-26 13:28:54 +0200

    Created project

---
+++ src/MiniSuave/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("MiniSuave")>]
+[<assembly: AssemblyProductAttribute("MiniSuave")>]
+[<assembly: AssemblyDescriptionAttribute("Creating a miniture version of Suave. Chapter 1 in F# Applied")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
+    let [<Literal>] InformationalVersion = "1.0"
