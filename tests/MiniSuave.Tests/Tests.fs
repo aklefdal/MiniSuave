7a9d44d01d56ea4a7dba65e7588f2895231a1211
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2016-07-26 14:50:05 +0200

    OK function implemented

--- tests/MiniSuave.Tests/Tests.fs
+++ tests/MiniSuave.Tests/Tests.fs
@@ -1,10 +1,20 @@
 module MiniSuave.Tests
 
-open MiniSuave
+open Suave.Http
+open Suave.Successful
 open NUnit.Framework
 
+let request = {Route = ""; Type = Suave.Http.GET}
+let response = {Content = ""; StatusCode = 200}
+let context = {Request = request; Response = response}
+
 [<Test>]
-let ``hello returns 42`` () =
-  let result = Library.hello 42
-  printfn "%i" result
-  Assert.AreEqual(42,result)
+let ``OK returns 200`` () =
+  let result = Suave.Successful.OK "" context |> Async.RunSynchronously
+  Assert.AreEqual(200, result.Value.Response.StatusCode)
+
+[<Test>]
+let ``OK returns content`` () =
+  let content = System.Guid.NewGuid().ToString()
+  let result = Suave.Successful.OK content context |> Async.RunSynchronously
+  Assert.AreEqual(content, result.Value.Response.Content)
